62de8f662eb391b3c0e219a5d81a9ad676e98abb
Sergio Arnese <sergio.arnese@rgigroup.com>
2020-12-02 19:41:11 +0100

    really begin of project structure

---
+++ ProgettoPilota/src/it/sergio/projects/Pilot.java
@@ -0,0 +1,12 @@
+package it.sergio.projects;
+
+import it.sergio.projects.sub.Subtraction;
+
+public class Pilot {
+	public static void main(String[] args) {
+		Subtraction subtraction = new Subtraction();
+		
+		System.out.println(subtraction.toString());
+		
+	}
+}
---
+++ ProgettoPilota/src/it/sergio/projects/sub/Subtraction.java
@@ -0,0 +1,5 @@
+package it.sergio.projects.sub;
+
+public class Subtraction {
+
+}
